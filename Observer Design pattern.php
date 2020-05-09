27612c07547c2b9c8e7dca42c58194cd3f10ac5e
RawanKS <45044954+RawanKS@users.noreply.github.com>
2020-05-09 02:52:20 +0200

    Add files via upload

---
+++ Observer Design pattern.php
@@ -0,0 +1,64 @@
+<?php
+
+abstract class Customer {
+    private $name;
+	abstract function send(Message $subject_in);
+}
+
+class User extends Customer {
+	public function __construct($name) {
+		$this->name = $name;
+	}
+	public function send(Message $subject) {
+		writeln(' Hello '.$this->name);
+		writeln(' '.$subject-> get_Text());    
+		writeln('');		
+	}
+}
+
+
+abstract class Message {
+	abstract function attatch(Customer $c);
+	abstract function detatch(Customer $c);
+	abstract function notify();
+}
+
+
+class actions extends Message {
+	private $text = NULL;
+	private $clients = array();
+	
+	function addClient(Customer $c) {
+		$this->clients[] = $c;
+	}
+	function removeClient(Customer $c) {
+		foreach($this->clients as $i => $client) {
+			if ($client === $c) { 
+				unset($this->clients[$i]);
+			}
+		}
+	}
+	function notify() {
+		foreach($this->clients as $client) {
+			$client->send($this);
+		}
+	}
+	function sendEmail($text) {
+		$this->text = $text;
+		$this->notify();
+	}
+	public function get_Text() {
+		return $this->text;
+	}
+}
+
+
+
+$clients = new clientGroup();
+$clients->sendEmail('welcome to the system');
+
+
+$clients->sendEmail('Nice to have you');
+
+
+?>
