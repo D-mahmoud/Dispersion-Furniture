6e8d9b4c727ae287357ddfa330fb51fda30ec5c7
samiraRefaat <44087134+samiraRefaat@users.noreply.github.com>
2020-05-10 11:21:51 +0200

    Update products.php

--- app/model/products.php
+++ app/model/products.php
@@ -5,12 +5,14 @@ require_once(__ROOT__ . "model/product.php");
 class Products extends Model {
     private $products;
     private $products_search;
-    private $dbh;
+    private $products_details;
     private $k;
+    private $k2;
 
 	function __construct() {
         $this->fillArray();
         $this->fillArray_search();
+        $this->fillArray_details();
     }
 
    	function fillArray() {
@@ -26,9 +28,12 @@ class Products extends Model {
 	}
 
 	function readProducts(){
+        $dbh = DBh::getInstance();
+        $mysqli = $dbh->getConnection();   
 		$sql = "SELECT * FROM product";
+		$result =	$mysqli->query($sql);
 
-		$result = $this->dbh->query($sql);
+		//$result = $this->dbh->query($sql);
 		if ($result->num_rows > 0){
 			return $result;
 		}
@@ -36,16 +41,64 @@ class Products extends Model {
 			return false;
 		}
     }
+    function delete($id){
+	//echo $id;
+     $dbh = DBh::getInstance();
+     $mysqli = $dbh->getConnection();  
+       // $sql="Delete from product where id='$id' ";
+      $sql=" DELETE FROM product WHERE  id='$id'";
+       
+        $result =	$mysqli->query($sql);
 
+        //$result = DBh::getInstance()->getConnection()->query($sql);
+          if($result  == true){
+                echo "deleted successfully.";
+                     return $result;  
+            } 
+            else{
+                echo "ERROR: Could not able to execute $sql. " .  $dbh->getConnection()->error;
+            }
+        }
+
+function details($id){
+   
+        $this->k2=$id;
+    $dbh = DBh::getInstance();
+    $mysqli = $dbh->getConnection();
+    $sql = "SELECT * FROM product where id='$id' ";  
+    $result =	$mysqli->query($sql);
+    if($result== true){
+     return $result;  
     
+    }else{
+        echo $id;
+    } 
+  
+}
+function fillArray_details() {
+    $this->products_details = array();
+    $this->dbh = $this->connect();
+    $result = $this->details($this->k2);
+    while ($row = $result->fetch_assoc()) {
+        array_push($this->products_details, new Product($row["id"],$row["name"],$row["type"],$row["code"],$row["cost"],$row["picture"]));
+    }
+}
+
+function getproducts_details() {
+    return $this->products_details;
+    
+} 
+
     function search($key)
     {   
     if(!empty($_POST["key"])){
     $key=$_REQUEST['key'];
     $this->k=$key;
+    $dbh = DBh::getInstance();
+    $mysqli = $dbh->getConnection();  
 
     $sql="(SELECT * FROM product WHERE ( type LIKE '%".$_POST["key"]."%') OR ( code LIKE '%".$_POST["key"]."%') OR (name LIKE '%".$_POST["key"]."%') OR (cost LIKE '%".$_POST["key"]."%'))" ;
-    $result = $this->dbh->query($sql);
+		$result =	$mysqli->query($sql);
     if($result== true){
     if ($result->num_rows > 0){
 
@@ -58,21 +111,21 @@ class Products extends Model {
         return $this->readProducts() ;
     }
 }else{
-    echo "ERROR: Could not able to execute $sql. " .  $this->dbh->getConn()->error;
+    echo "ERROR: Could not able to execute $sql. " .  $dbh->getConnection()->error;
 } 
 }
 else {
     $key="";
     $this->k=$key;
+    $dbh = DBh::getInstance();
+    $mysqli = $dbh->getConnection(); 
     $sql = "SELECT * FROM product";
-
-		$result = $this->dbh->query($sql);
+    $result =	$mysqli->query($sql);
 			return $result;
 		}
 }
 
 function fillArray_search() {
-    
     $this->products_search = array();
     $this->dbh = $this->connect();
     $result = $this->search($this->k);
@@ -85,17 +138,32 @@ function getProducts_search() {
     return $this->products_search;
     
 }
-function insertProduct($type,$name,$cost,$picture,$price){
- $sql = "INSERT INTO product (type, name,cost,picture,price) VALUES ('$type', '$name','$cost','$picture','$price')";
-        if($this->dbh->query($sql) === true){
+function insertProduct($name,$type,$code,$cost,$picture){
+$dbh = DBh::getInstance();
+        $mysqli = $dbh->getConnection();   
+		$sql="INSERT INTO product (name, type,code,cost,picture) VALUES ('$name', '$type','$code','$cost','$picture')";
+		 $result =	$mysqli->query($sql);
+		if( $result === true){
             echo "Records inserted successfully.";
             $this->fillArray();
         } else{
             echo "ERROR: Could not able to execute $sql. " .  $this->dbh->getConn()->error;
         }  
-      }
+}
+function editProduct($id,$name,$type,$code,$cost,$picture){
+
+ $mysqli = $dbh->getConnection();   
+		$sql="UPDATE product  SET name='$name',type='$type',code='$code',cost='$cost',picture='$picture'
+		WHERE id='$id'";
+		 $result =	$mysqli->query($sql);
+		if( $result === true){
+            echo "Records inserted successfully.";
+            $this->fillArray();
+        } else{
+            echo "ERROR: Could not able to execute $sql. " .  $this->dbh->getConn()->error;
+        }  
 
 }
-		 
+}		 
 	  
 	
