ff5c1c4a3d44dfa09f5bc1cb49108401398bd1a8
shahdhesham <44087178+shahdhesham@users.noreply.github.com>
2020-06-04 13:08:35 +0200

    Update signup.php

--- public/signup.php
+++ public/signup.php
@@ -4,16 +4,23 @@ define('__ROOT__', "../app/");
 require_once(__ROOT__ . "model/Users.php");
 require_once(__ROOT__ . "controller/UsersController.php");
 require_once(__ROOT__ . "view/Register.php");
+require_once(__ROOT__ . "view/Admin.php");
+
 
 $model = new Users();
 $controller = new UsersController($model);
 $view = new Register($controller, $model);
+$view_admin= new Admin($controller, $model);
+
 
 if (isset($_GET['action']) && !empty($_GET['action'])) {
     $controller->{$_GET['action']}();
    
     
 }
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -38,11 +45,32 @@ if (isset($_GET['action']) && !empty($_GET['action'])) {
             <div class="col-lg-12">
                     <div class="login_part_form">
                         <div class="login_part_form_iner">
+                       <?php
+                        if  (empty($_SESSION['role']))
+                        {?>
+                            <h3>Welcome! <br>
+                            Lets sign up</h3>
+                            <?php
+                            echo $view->signup();
+                        }
+                        else{
+                        if (($_SESSION['role']) == 'customer' )
+                         {?>
                             <h3>Welcome! <br>
                                 Lets sign up</h3>
                                 <?php
                                 echo $view->signup();
-                                ?>
+                                } 
+                                else{?>
+                                 <h3>Welcome! <br>
+                               sign up a new Employee</h3>
+                                <?php
+                                    echo $view_admin->signup();
+   
+                                }
+                            }?>
+                                
+
                                 </div>
                             </form>
                         </div>
