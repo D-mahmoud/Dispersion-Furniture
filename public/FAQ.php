417eb6fa1fbd9aaf02911d6feb88a44eae3f84de
shahdhesham <44087178+shahdhesham@users.noreply.github.com>
2020-06-09 00:05:21 +0200

    Add files via upload

---
+++ public/FAQ.php
@@ -0,0 +1,73 @@
+<?php
+
+include "../extra/header.php" ;
+?>
+ <!-- breadcrumb part start-->
+ <section class="breadcrumb_part">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb_iner">
+                        <h2>Help </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb part end-->
+
+    <div class="container">
+   
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="order_details_iner">
+		  <br>
+            <h3>FAQ</h3>
+            <table class="table table-borderless">
+             
+                 <tr>
+                  <th scope="col" >1.How to do a purchase?</th>
+                  <td> you need to have an account </td> 
+
+                </tr>
+                <tr>
+                  <th scope="col" >2.How to creat an account?</th>
+                  <td> You can try login then choose create acount button </td> 
+
+                </tr>
+                <tr>
+                  <th scope="col" >3.If there is an issue wih my order</th>
+                  <td>Please send us a message and clarify your problem</td> 
+
+                </tr>
+                <tr>
+                  <th scope="col" >4.My order has  been dissaproved what should i do?</th>
+                  <td>You can try ordering again after sometime</td> 
+
+                </tr>
+                <tr>
+                  <th scope="col" >5. Why my order has  been dissaproved?</th>
+                  <td>We apologize for this,but we are incapable of doung yout order now</td> 
+
+                </tr>
+                <tr>
+                  <th scope="col" >5. Why my order has not been approved yet?</th>
+                  <td>Give it time .we will see your request as sonnas we can</td> 
+
+                </tr>
+          </table>
+          <br><br>
+          <h3>contact Us</h3>
+          <p><h4>Email</h4>Dispersion@gmail.com
+          <h4>Phone</h4>+210#########</p>
+
+        </div>
+     </div>
+
+
+</html>
+
+
+
+
+                
