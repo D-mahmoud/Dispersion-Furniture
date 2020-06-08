56828490bc199a9f3bc7e25e432298ecc354461c
shahdhesham <44087178+shahdhesham@users.noreply.github.com>
2020-06-08 12:14:41 +0200

    Update cart.php

--- public/cart.php
+++ public/cart.php
@@ -1,28 +1,46 @@
 <?php
-//index.php
-//include autoloader
-require_once 'dompdf/autoload.inc.php';
 
-// reference the Dompdf namespace
+define('__ROOT__', "../app/");
+require_once(__ROOT__ . "model/Carts.php");
+require_once(__ROOT__ . "controller/CartController.php");
+require_once(__ROOT__ . "view/Shop.php");
 
-use Dompdf\Dompdf;
+$model = new Carts();
+$controller = new CartController($model);
+$view = new Shop($controller, $model);
 
-//initialize dompdf class
-
-$document = new Dompdf();
-
-$document->loadHtml("this is pdf");
-
-//set page size and orientation
-
-$document->setPaper('A4', 'landscape');
-
-//Render the HTML as PDF
-
-$document->render();
-//Get output of generated pdf in Browser
-
-$document->stream("Webslesson", array("Attachment"=>0));
-//1  = Download
-//0 = Preview
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+   
+    
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php include "../extra/header.php" ?>
+
+ <!-- breadcrumb part start-->
+ <section class="breadcrumb_part">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb_iner">
+                        <h2>cart list</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb part end-->
+
+                                <?php
+                                echo $view->viewcart();
+                                ?>
+                                <div class="checkout_btn_inner float-right">
+                               
+        </div>
+        </div>
+    </div>
+</section>
+	   		<?php include "../extra/footer.html" ?>  
