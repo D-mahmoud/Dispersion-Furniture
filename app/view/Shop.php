d52fad12dc8b8601856731e33da32c06cbf80b0a
shahdhesham <44087178+shahdhesham@users.noreply.github.com>
2020-06-08 12:03:06 +0200

    Update Shop.php

--- app/view/Shop.php
+++ app/view/Shop.php
@@ -3,7 +3,6 @@ require_once(__ROOT__ . "view/View.php");
 class Shop extends View{
     public function output()
 
-    
     {
         $str="";
         $i=2;
@@ -25,6 +24,9 @@ class Shop extends View{
     }
     return $str;
     }
+    
+			
+
 public function search(){
     $str='
     <form action="explore.php?action=search" method="post">
@@ -34,19 +36,7 @@ public function search(){
 return $str;
 
 }
-public function search_output(){
-    $str=""; 
-    
-    foreach($this->model->getProducts_search() as $product){
-        $str= $str . '<div class="col-lg-6 col-sm-6">';
-        $str .=' <div class="single_product_item">';
-        $str .='<img src=    ../img/product/' . $product->getpicture(). 'class="img-fluid">';
-        $str .='<h3> <p>'.$product->getCost().'</p>';
-        $str .='<a href="single-product.html">'.$product->getName().'</a> </h3> </div> </div>';
-    }
-  # $str=$str. $this->model->getProducts_search() ;
-return $str;
-}
+
 public function single_ptoduct()
 {$str="";
     foreach($this->model->getproducts_details() as $product){      
@@ -72,141 +62,32 @@ public function single_ptoduct()
  <?php	}
 }';
 
-//test
+
 
 }
 }
 
-   
-
-public function viewcart(){
-
-    $str='
-    <section class="cart_area section_padding">
-    <div class="container">
-      <div class="cart_inner">
-        <div class="table-responsive">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Product</th>
-                <th scope="col">Price</th>
-                <th scope="col">Quantity</th>
-                <th scope="col">Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>';
-              $i=3;
-              foreach(array_slice($this->model->getCarts(), 0, $i)as $cart){
-                $str= $str .'<td>';
-                    $str .='<h3> <p>'.$cart->getproductname().'</p>';
-                     $str= $str .'</td>
-                <td>';
-                $str .='<h3> <p>'.$cart->getcost().'</p>';
-                
-                $str= $str .'</td>
-                <td>
-                 ';
-                 $str .='<h3> <p>'.$cart->getquantity().'</p>';  
-                  
-                 $str= $str .'</td>
-                    
-                  
-                   
-                  </div>
-                </td>
-                <td>';
-                $str .='<h3> <p>'.$cart->getcost().'</p>   </td>
-              </tr>';
-
-               } $str= $str .' 
-                 
-              
-             
-              <tr class="bottom_button">
-                <td>
-                  <a class="btn_1" href="#">Update Cart</a>
-                </td>
-                <td></td>
-                <td></td>
-                <td>
-                  <div class="cupon_text float-right">
-                    <a class="btn_1" href="#">Close Coupon</a>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td></td>
-                <td></td>
-                <td>
-                  <h5>Subtotal</h5>
-                </td>
-                <td>
-                  <h5>$2160.00</h5>
-                </td>
-              </tr>
-              <tr class="shipping_area">
-                <td></td>
-                <td></td>
-                <td>
-                  <h5>Shipping</h5>
-                </td>
-                <td>
-                  <div class="shipping_box">
-                    <ul class="list">
-                      <li>
-                        Flat Rate: $5.00
-                        <input type="radio" aria-label="Radio button for following text input">
-                      </li>
-                      <li>
-                        Free Shipping
-                        <input type="radio" aria-label="Radio button for following text input">
-                      </li>
-                      <li>
-                        Flat Rate: $10.00
-                        <input type="radio" aria-label="Radio button for following text input">
-                      </li>
-                      <li class="active">
-                        Local Delivery: $2.00
-                        <input type="radio" aria-label="Radio button for following text input">
-                      </li>
-                    </ul>
-                    <h6>
-                      Calculate Shipping
-                      <i class="fa fa-caret-down" aria-hidden="true"></i>
-                    </h6>
-                    <select class="shipping_select">
-                      <option value="1">Cairo</option>
-                      <option value="2">India</option>
-                      <option value="4">Pakistan</option>
-                    </select>
-                    <select class="shipping_select section_bg">
-                      <option value="1">Select a State</option>
-                      <option value="2">Select a State</option>
-                      <option value="4">Select a State</option>
-                    </select>
-                    <input class="post_code" type="text" placeholder="Postcode/Zipcode" />
-                    <a class="btn_1" href="#">Update Details</a>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <div class="checkout_btn_inner float-right">
-            <a class="btn_1" href="#">Continue Shopping</a>
-            <a class="btn_1 checkout_btn_1" href="#">Proceed to checkout</a>
-          </div>
-        </div>
-      </div>
-  </section>
-';
-return $str;
-
-              }
-     public function feature()
-{
+public function search_output(){
+    $str=""; 
     
+    foreach($this->model->getProducts_search() as $product){
+        $str= $str . '<div class="col-lg-6 col-sm-6">';
+        $str .=' <div class="single_product_item">';
+        $str .='<img src=../img/product/' . $product->getpicture() . ' </img> ' ;
+        $str .='<h3> <p>'.$product->getCost().'EPG </p></h3>';
+       $str .='
+       <form  action="single-product.php?action=details" method="post">
+       <button type="submit" class="btn_3" >'.$product->getName().'</button>  </div> </div>
+       <input type="hidden" name="id"  value='.$product->getID().'>
+
+       </form>';
+    
+    }
+    return $str;
+    }
+
+    public function feature()
+{
 $str='
     <section class="feature_part section_padding">
     <div class="container">
@@ -256,4 +137,122 @@ return $str;
 
 }
 
+public function cart_button($id){
+  $str='<form  action=cart.php?action=insert  method="post">
+  
+ <div class="product_count d-inline-block">
+  <span class="product_count_item inumber-decrement"> <i class="ti-minus"></i></span>
+  <input class="product_count_item input-number" type="text" value="1" min="0" max="10"  name="amount">
+  <span class="product_count_item number-increment"> <i class="ti-plus"></i></span>
+</div>
+<br><br>
+        <input type="hidden" name="id"  value='.$id.'>
+        <button type="submit" name="submit" class="btn_3" >Add to cart</button>  
+        </form>';
+    return $str;
+}
+
+/*public function order(){
+  foreach($this->model->getCarts()as $cart){
+    if($cart->getuserid()==$_SESSION['ID']){
+      if($cart->getstatus()==""){
+  $str='<form  action=cart.php?action=order  method="post">
+
+  <input type="hidden" name="product_id"  value='.$cust->getproductid().'>
+  <input type="hidden" name="user_id"  value='.$cust->getuserid().'>
+  <input type="hidden" name="quantity"  value='.$cust->getquantity().'>
+  <input type="hidden" name="total"  value='. $cust->getCost() * $cust->getquantity().'>
+
+  <button type="submit">Place Order</button>
+  </form>';
+  return $str;
+      }}
+}
+}*/
+
+public function viewcart(){
+
+  $str='
+  <section class="cart_area section_padding">
+  <div class="container">
+    <div class="cart_inner">
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Product</th>
+              <th scope="col">Price</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Total</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>';
+           
+
+            foreach($this->model->getCarts()as $cart){
+              if($cart->getuserid()==$_SESSION['ID'] &&  $cart->getcart_id()==""){
+                // if($cart->getstat()==""){
+              $str= $str .'<td>';
+              $str .='<h3> <p>'.$cart->getproductname().'</p>';
+                   $str= $str .'</td>
+                   
+              <td>';
+              
+              $str .='<h3> <p>'.$cart->getcost().'</p>';
+              
+              $str= $str .'</td>
+              <td>
+               ';
+               $str .='<h3> <p>'.$cart->getquantity().'</p>';  
+                
+               $str= $str .'</td>
+                  
+                
+                 
+                </div>
+              </td>
+
+              <td>';
+              $str .='<h3> <p>'.$cart->getcost()*$cart->getquantity().'</p>   </td>';
+            $str .= ' <td> ' .' <form  action="status.php?action=req" method="post">
+              <input type="radio" id="order" name="check" value="order">
+              <label for="order"><h6>order</h6></label>
+              <input type="radio" id="cancel" name="check" value="cancel">
+              <label for="cancel"><h6>cancel</h6></label>
+      
+            
+              <input type="hidden" name="id"  value='. $cart->getID().'>
+      
+              <input type="submit" value="Submit">
+              </form>';
+              
+              $str .= " </td> ".
+            '</tr>'; 
+            
+          // }
+          }
+            }$str= $str .' 
+               
+            
+           
+            <tr class="bottom_button">
+              <td>
+              <a class="btn_1" href="explore.php">Continue Shopping</a>
+              </td>
+              <td></td>
+              <td></td>
+              
+            </tr>
+            
+          </tbody>
+        </table>
+        
+  
+';
+return $str;
+
+
+              }
             }
